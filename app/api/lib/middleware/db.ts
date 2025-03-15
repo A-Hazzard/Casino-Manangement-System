b7cf8a4ccec67a6ba287a471a177c6f0c30b2315
A-Hazzard <aaronsploit@gmail.com>
2025-03-15 11:40:31 -0400

    refactor

--- app/api/lib/middleware/db.ts
+++ app/api/lib/middleware/db.ts
@@ -1,18 +1,19 @@
 // lib/middleware/db.ts
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/";
 if (!MONGO_URI) {
     throw new Error("❌ MONGO_URI not set in environment variables");
 }
 
 export async function connectDB() {
     try {
-        await mongoose.connect(MONGO_URI, {
+        const connection = await mongoose.connect(MONGO_URI, {
             bufferCommands: false,
             connectTimeoutMS: 30000,
         });
         console.log("🔥 MongoDB connected successfully.");
+        return connection.connection; // Make sure to return the actual connection
     } catch (err) {
         console.error("❌ MongoDB connection error:", err);
         throw err;
